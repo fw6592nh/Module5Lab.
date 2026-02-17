885432c5559f7e3a4c4ade7ba40525ee248f4db6
Ducelle Roscane Massembo Banzouzi <fw6592nh@go.minnstate.edu>
2026-02-17 17:54:21 +0000

    new commit

---
+++ Module5Lab/Program.cs
@@ -0,0 +1,16 @@
+﻿using Module5Lab;
+
+//Create a new player with one life and add points to the player's score
+Player player = new Player("Frankenstein", 1);
+player.AddPoints(100);
+
+//Display the player's score
+Console.WriteLine("Welcome to the game! Your score is: " + player.GetScore() + 
+    " You have " + player.GetLivesLeft() + " lives left!");
+
+//Kill off the player's character
+player.Kill();
+
+//Let the player know their character was killed
+Console.WriteLine("An orc attacks you. Sorry, you were killed. You have " 
+    + player.GetLivesLeft() + " lives left!");
